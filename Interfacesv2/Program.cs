72756170b54437c9248ccbe04be20a7bd91d5506
Paul Chronakis <paul.chronakis@Peoplecert.onmicrosoft.com>
2021-02-02 16:02:40 +0200

    Add project files.

---
+++ Interfacesv2/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfacesv2
+{
+    class Program
+    {
+
+        interface IItem
+        {
+            string name { get; set; }
+            int goldValue { get; set; }
+            void Equip();
+            void Sell();
+        }
+
+        interface IDamagable
+        {
+            int durability { get; set; }
+            void TakeDamage(int _amount);
+        }
+
+        interface IPartOfQuest
+        {
+            void TurnIn();
+        }
+
+        class Sword : IItem, IDamagable, IPartOfQuest
+        {
+            public string name { get; set; }
+            public int goldValue { get; set; }
+            public int durability { get; set; }
+            public Sword(string _name)
+            {
+                name = _name;
+                goldValue = 100;
+                durability = 30;
+            }
+
+            public void Equip()
+            {
+                Console.WriteLine(name + " equipped.");
+            }
+            public void Sell()
+            {
+                Console.WriteLine(name + " sold for " + goldValue + " adena!");
+            }
+
+            public void TakeDamage (int _dmg)
+            {
+                durability -= _dmg;
+                Console.WriteLine(name + " damaged by  " + _dmg + 
+                    ". It now has a durability of " + durability);
+            }
+
+            public void TurnIn()
+            {
+                Console.WriteLine(name + " turned in");
+            }
+
+        }
+
+        class Axe : IItem, IDamagable, IPartOfQuest
+        {
+            public string name { get; set; }
+            public int goldValue { get; set; }
+            public int durability { get; set; }
+            public Axe(string _name)
+            {
+                name = _name;
+                goldValue = 70;
+                durability = 50;
+            }
+
+            public void Equip()
+            {
+                Console.WriteLine(name + " equipped.");
+            }
+            public void Sell()
+            {
+                Console.WriteLine(name + " sold for " + goldValue + " adena!");
+            }
+
+            public void TakeDamage(int _dmg)
+            {
+                durability -= _dmg;
+                Console.WriteLine(name + " damaged by  " + _dmg +
+                    ". It now has a durability of " + durability);
+            }
+            public void TurnIn()
+            {
+                Console.WriteLine(name + " turned in");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Sword sword = new Sword("Sword of destiny");
+            sword.Equip();
+            sword.TakeDamage(20);
+            sword.Sell();
+            
+
+            Console.WriteLine();
+
+            Axe axe = new Axe("Fury Axe");
+            axe.Equip();
+            axe.TakeDamage(20);
+            axe.Sell();
+
+            Console.WriteLine();
+
+            //Create an inventory
+            IItem[] inventory = new IItem[2];
+            inventory[0] = sword;
+            inventory[1] = axe;
+
+            //Lopp throught and turn in all quest items
+            for (int i = 0; i < inventory.Length; i++)
+            {
+                IPartOfQuest questItem = inventory[i] as IPartOfQuest;
+                if (questItem != null)
+                {
+                    questItem.TurnIn();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
